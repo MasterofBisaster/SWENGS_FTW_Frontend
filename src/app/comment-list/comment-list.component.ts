38bebab6185f3876eea6f93aedda06070cc08489
julian <julian.delazer@edu.fh-joanneum.at>
2020-01-15 16:52:58 +0100

    Configuration of Comment-List

--- src/app/comment-list/comment-list.component.ts
+++ src/app/comment-list/comment-list.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../service/user.service';
 import {CommentService} from '../service/comment.service';
+import {VirtualScrollerModule} from 'primeng/virtualscroller';
 
 @Component({
     selector: 'app-comment-list',
     templateUrl: './comment-list.component.html',
-    styleUrls: ['./comment-list.component.scss']
+    styleUrls: ['./comment-list.component.scss'],
 })
 export class CommentListComponent implements OnInit {
 
