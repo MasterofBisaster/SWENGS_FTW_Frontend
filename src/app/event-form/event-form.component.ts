bb1b0ad55aa41901871597cb9180bdae61f989e0
Michael Zollweg <michael.zollweg@edu.fh-joanneum.at>
2020-01-16 17:14:09 +0100

    fix create Event

--- src/app/event-form/event-form.component.ts
+++ src/app/event-form/event-form.component.ts
@@ -9,60 +9,60 @@ import {EventService} from '../service/event.service';
 
 
 @Component({
-    selector: 'app-event-form',
-    templateUrl: './event-form.component.html',
-    styleUrls: ['./event-form.component.scss']
+  selector: 'app-event-form',
+  templateUrl: './event-form.component.html',
+  styleUrls: ['./event-form.component.scss']
 })
 export class EventFormComponent implements OnInit {
-    eventFormGroup;
-    categoryOptions;
-    commentOptions;
+  eventFormGroup;
+  categoryOptions;
+  commentOptions;
 
-    constructor(private fb: FormBuilder, private route: ActivatedRoute, private router: Router,
-                private categoryService: CategoryService, private commentService: CommentService,
-                private locationService: LocationService, private userService: UserService,
-                private eventService: EventService) {
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private router: Router,
+              private categoryService: CategoryService, private commentService: CommentService,
+              private locationService: LocationService, private userService: UserService,
+              private eventService: EventService) {
+  }
+
+  ngOnInit() {
+
+    this.eventFormGroup = this.fb.group({
+      id: [null],
+      name: ['', [Validators.required]],
+      start_date: [null, [Validators.required]],
+      end_date: [null, [Validators.required]],
+      private: [null, [Validators.required]],
+      location: [null, [Validators.required]],
+      category: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+      max_users: [null],
+      confirmed_users: [null],
+      costs: [null],
+      picture: [],
+      creator: [null]
+    });
+
+    const data = this.route.snapshot.data;
+    this.categoryOptions = data.categoryOptions;
+    this.commentOptions = data.commentOptions;
+
+    if (data.event) {
+      this.eventFormGroup.patchValue(data.event);
     }
+  }
 
-    ngOnInit() {
-
-        const data = this.route.snapshot.data;
-        this.categoryOptions = data.categoryOptions;
-        this.commentOptions = data.commentOptions;
-
-        this.eventFormGroup = this.fb.group({
-            id: [null],
-            name: ['', [Validators.required]],
-            start_date: [null, [Validators.required]],
-            end_date: [null, [Validators.required]],
-            private: [null, [Validators.required]],
-            location: [null, [Validators.required]],
-            category: [null, [Validators.required]],
-            description: [null, [Validators.required]],
-            max_users: [null],
-            confirmed_users: [null],
-            costs: [null],
-            picture: [],
-            creator: [null]
+  createEvent() {
+    const event = this.eventFormGroup.value;
+    if (event.id) {
+      this.eventService.updateEvent(event)
+        .subscribe(() => {
+          alert('updated successfully');
         });
-
-        if (data.event) {
-            this.eventFormGroup.patchValue(data.event);
-        }
+    } else {
+      this.eventService.createEvent(event)
+        .subscribe((response: any) => {
+          this.router.navigate(['/event-form/' + response.id]);
+        });
     }
-
-    createMovie() {
-        const event = this.eventFormGroup.value;
-        if (event.id) {
-            this.eventService.updateEvent(event)
-                .subscribe(() => {
-                    alert('updated successfully');
-                });
-        } else {
-            this.eventService.createEvent(event)
-                .subscribe((response: any) => {
-                    this.router.navigate(['/event-form/' + response.id]);
-                });
-        }
-    }
+  }
 }
