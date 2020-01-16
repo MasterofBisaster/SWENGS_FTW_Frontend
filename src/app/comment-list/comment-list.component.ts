a8e440cbdf28d0ce91bcbc48d42c80c3105e7014
Urb4nOutl4w <45825633+Urb4nOutl4w@users.noreply.github.com>
2020-01-16 13:14:58 +0100

    added comment form, event deatail and combined them with comment list

--- src/app/comment-list/comment-list.component.ts
+++ src/app/comment-list/comment-list.component.ts
@@ -1,31 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {UserService} from '../service/user.service';
 import {CommentService} from '../service/comment.service';
 import {VirtualScrollerModule} from 'primeng/virtualscroller';
 
 @Component({
-    selector: 'app-comment-list',
-    templateUrl: './comment-list.component.html',
-    styleUrls: ['./comment-list.component.scss'],
+  selector: 'app-comment-list',
+  templateUrl: './comment-list.component.html',
+  styleUrls: ['./comment-list.component.scss'],
 })
 export class CommentListComponent implements OnInit {
 
-    comments: any[];
+  comments: any[];
+  @Input() eventId: number;
 
-    constructor(private commentService: CommentService, public userService: UserService) {
-    }
+  constructor(private commentService: CommentService, public userService: UserService) {
+  }
 
-    ngOnInit() {
-        this.commentService.getComments()
-            .subscribe((response: any[]) => {
-                this.comments = response;
-            });
-    }
+  ngOnInit() {
+    this.commentService.getComments()
+      .subscribe((response: any[]) => {
+        this.comments = response;
+      });
+  }
 
-    deleteComment(comments: any) {
-        this.commentService.deleteComment(comments)
-            .subscribe(() => {
-                this.ngOnInit();
-            });
-    }
+  deleteComment(comments: any) {
+    this.commentService.deleteComment(comments)
+      .subscribe(() => {
+        this.ngOnInit();
+      });
+  }
 }
