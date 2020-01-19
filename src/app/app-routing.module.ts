54901be0ea9ee4e76ee10443e48cf1cd46313658
Urb4nOutl4w <45825633+Urb4nOutl4w@users.noreply.github.com>
2020-01-19 17:34:54 +0100

    tried to fix not reload bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -84,6 +84,7 @@ const routes: Routes = [
   {
     path: 'user-detail/:id',
     component: UserDetailComponent,
+    runGuardsAndResolvers: 'always',
     resolve: {
       user: FtwUserDetailResolver,
     }
@@ -111,7 +112,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    onSameUrlNavigation: 'reload'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
