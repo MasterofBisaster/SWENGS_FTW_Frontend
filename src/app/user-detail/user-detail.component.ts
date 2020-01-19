ea460733406e37b14f23ba3014a6d9cb79884656
julian <julian.delazer@edu.fh-joanneum.at>
2020-01-19 14:30:37 +0100

    Fix user/ftwuser

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {FormBuilder, Validators} from '@angular/forms';
+import {CategoryService} from '../service/category.service';
+import {HttpClient} from '@angular/common/http';
+import {UserService} from '../service/user.service';
 
 @Component({
     selector: 'app-user-detail',
@@ -9,8 +12,11 @@ import {FormBuilder, Validators} from '@angular/forms';
 })
 export class UserDetailComponent implements OnInit {
     ftwUserDetailGroup;
+    newPicture;
+    fileToUpload: File = null;
 
-    constructor(private route: ActivatedRoute, private fb: FormBuilder) {
+    constructor(private route: ActivatedRoute, private fb: FormBuilder, private http: HttpClient,
+                private userService: UserService) {
     }
 
     ngOnInit() {
@@ -30,4 +36,24 @@ export class UserDetailComponent implements OnInit {
         }
     }
 
+    uploadFile(event) {
+        const files = event.files;
+        this.fileToUpload = files.item(0);
+        this.postFile(this.fileToUpload).subscribe((response: any) => {
+            this.newPicture = response;
+            this.ftwUserDetailGroup.get('picture').patchValue(this.newPicture.id);
+            const user = this.ftwUserDetailGroup.value;
+            this.userService.updateUser(user)
+                .subscribe(() => {
+                    window.location.reload();
+                });
+        });
+    }
+
+    postFile(fileToUpload: File) {
+        const endpoint = '/api/media/upload';
+        const formData: FormData = new FormData();
+        formData.append('file', fileToUpload, fileToUpload.name);
+        return this.http.post(endpoint, formData);
+    }
 }
