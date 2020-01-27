0ce375c239913a1dd646773ee544977ed961d87a
Urb4nOutl4w <45825633+Urb4nOutl4w@users.noreply.github.com>
2020-01-27 09:11:51 +0100

    fixed friend display bug at profile

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -6,91 +6,93 @@ import {UserService} from '../service/user.service';
 import {MessageService} from 'primeng/api';
 
 @Component({
-    selector: 'app-user-detail',
-    templateUrl: './user-detail.component.html',
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.scss'],
   providers: [MessageService]
 
 })
 export class UserDetailComponent implements OnInit {
-    ftwUserDetailGroup;
-    newPicture;
-    fileToUpload: File = null;
-    picture;
+  ftwUserDetailGroup;
+  newPicture;
+  fileToUpload: File = null;
+  picture;
   userFriendsBool;
   friends: any[];
-    constructor(private route: ActivatedRoute, private fb: FormBuilder, private http: HttpClient,
-                private userService: UserService, private messageService: MessageService) {
+
+  constructor(private route: ActivatedRoute, private fb: FormBuilder, private http: HttpClient,
+              private userService: UserService, private messageService: MessageService) {
+  }
+
+  ngOnInit() {
+
+    this.ftwUserDetailGroup = this.fb.group({
+      id: [null],
+      picture: [null],
+      user_username: [null],
+      first_name: [null],
+      last_name: [null],
+      user_id: [null],
+    });
+    const data = this.route.snapshot.data;
+
+    this.setFtwUser();
+
+    this.route.params.subscribe((params: { filter: string }) => {
+      this.setFtwUser();
+    });
+    if (this.userService.userId() != null) {
+      this.userService.checkFriends(this.userService.userId(), this.ftwUserDetailGroup.controls.user_id.value).subscribe((response) => {
+        if (typeof response === 'string') {
+          this.userFriendsBool = Boolean(JSON.parse(response));
+        }
+      });
     }
+  }
 
-    ngOnInit() {
+  setFtwUser() {
+    const data = this.route.snapshot.data;
 
-        this.ftwUserDetailGroup = this.fb.group({
-            id: [null],
-            picture: [null],
-            user_username: [null],
-            first_name: [null],
-            last_name: [null],
-            user_id: [null],
+    if (data.user) {
+      this.ftwUserDetailGroup.patchValue(data.user);
+    }
+    if (data.friends) {
+      this.friends = data.friends;
+    }
+  }
+
+  uploadFile(event) {
+    const files = event.files;
+    this.fileToUpload = files.item(0);
+    this.postFile(this.fileToUpload).subscribe((response: any) => {
+      this.newPicture = response;
+      this.ftwUserDetailGroup.get('picture').patchValue(this.newPicture.id);
+      const user = this.ftwUserDetailGroup.value;
+      this.userService.updateUser(user)
+        .subscribe(() => {
+          window.location.reload();
         });
-        const data = this.route.snapshot.data;
-        if (data.friends) {
-        this.friends = data.friends;
-      }
-        this.setFtwUser();
+    });
+  }
 
-        this.route.params.subscribe((params: { filter: string }) => {
-            this.setFtwUser();
-        });
-        if (this.userService.userId() != null) {
-          this.userService.checkFriends(this.userService.userId(), this.ftwUserDetailGroup.controls.user_id.value).subscribe((response) => {
-            if (typeof response === 'string') {
-              this.userFriendsBool = Boolean(JSON.parse(response));
-            }
+  postFile(fileToUpload: File) {
+    const endpoint = '/api/media/upload';
+    const formData: FormData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+    return this.http.post(endpoint, formData);
+  }
+
+  addOrRemoveAsFriend() {
+    this.userService.addOrRemoveUserAsFriend(this.userService.userId(), this.ftwUserDetailGroup.controls.user_id.value)
+      .subscribe(() => {
+        if (this.userFriendsBool) {
+          this.messageService.add({severity: 'success', summary: 'Successfully added!', detail: 'You added this user as a friend!'});
+        } else {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Successfully removed', detail: 'You removed this user as a friend!'
           });
         }
-    }
-
-    setFtwUser() {
-        const data = this.route.snapshot.data;
-
-        if (data.user) {
-            this.ftwUserDetailGroup.patchValue(data.user);
-        }
-    }
-
-    uploadFile(event) {
-        const files = event.files;
-        this.fileToUpload = files.item(0);
-        this.postFile(this.fileToUpload).subscribe((response: any) => {
-            this.newPicture = response;
-            this.ftwUserDetailGroup.get('picture').patchValue(this.newPicture.id);
-            const user = this.ftwUserDetailGroup.value;
-            this.userService.updateUser(user)
-                .subscribe(() => {
-                    window.location.reload();
-                });
-        });
-    }
-
-    postFile(fileToUpload: File) {
-        const endpoint = '/api/media/upload';
-        const formData: FormData = new FormData();
-        formData.append('file', fileToUpload, fileToUpload.name);
-        return this.http.post(endpoint, formData);
-    }
-
-    addOrRemoveAsFriend() {
-        this.userService.addOrRemoveUserAsFriend(this.userService.userId(), this.ftwUserDetailGroup.controls.user_id.value)
-            .subscribe(() => {
-              if (this.userFriendsBool) {
-                this.messageService.add({severity: 'success', summary: 'Successfully added!', detail: 'You added this user as a friend!'});
-              } else {
-                this.messageService.add({
-                  severity: 'success',
-                  summary: 'Successfully removed', detail: 'You removed this user as a friend!'
-                });
-              }
-            });
-    }
+      });
+  }
 }
